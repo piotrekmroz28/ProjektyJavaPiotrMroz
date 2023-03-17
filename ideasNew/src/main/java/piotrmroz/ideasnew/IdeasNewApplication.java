97d88188d36db7ba2f2435707475327470fe32a8
piotrekmroz28 <piotrekmroz10@gmail.com>
2023-03-17 18:03:38 +0100

    Porządki w starych projektach, poprawa strony z portfolio

---
+++ ideasNew/src/main/java/piotrmroz/ideasnew/IdeasNewApplication.java
@@ -0,0 +1,13 @@
+package piotrmroz.ideasnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeasNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeasNewApplication.class, args);
+    }
+
+}
--- ideasNew/src/test/java/piotrmroz/ideasnew/IdeasNewApplicationTests.java
+++ ideasNew/src/test/java/piotrmroz/ideasnew/IdeasNewApplicationTests.java
@@ -1,13 +1,13 @@
-package piotrmroz.FirstSpring;
+package piotrmroz.ideasnew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstSpringApplicationTests {
+class IdeasNewApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
