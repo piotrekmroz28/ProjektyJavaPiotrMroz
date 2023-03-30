5aed5797f9b5280d9ae839959babeaec4f1ec1f4
piotrekmroz28 <piotrekmroz10@gmail.com>
2023-03-30 09:31:03 +0200

    dodanie starszych kodów

---
+++ Angular/easy-words-master/src/app/components/answers/answers.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/models';
+
+@Component({
+  selector: 'app-answers',
+  templateUrl: './answers.component.html',
+  styleUrls: ['./answers.component.css']
+})
+export class AnswersComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() words: WordType;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ PrakticeJava/Ex3_Text/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    private static int age = 25;
+    private static boolean a = true;
+    private static char inicjaly1 = 'J';
+    private static char inicjaly2 = 'K';
+    public static void main(String[] args) {
+        System.out.println("-Cześć Jan, słyszałem, że masz " + age + " lat i uczysz się programowania. Czy to prawda?");
+        System.out.println("- " + a);
+        System.out.println("-Czy Twoje inicjały to " + inicjaly1 + " " + inicjaly2 + "?");
+        System.out.println("-Tak, " + a);
+    }
+}
